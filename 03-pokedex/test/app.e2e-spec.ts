380bcafb1d14d606497fe6e0199e27f43adf215d
David Ostos <davidostos2@gmail.com>
2023-11-13 20:59:10 -0400

    comenzando proyecto 03 pokedex

--- 03-pokedex/test/app.e2e-spec.ts
+++ 03-pokedex/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { Test, type TestingModule } from '@nestjs/testing';
+import { type INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -8,15 +8,15 @@ describe('AppController (e2e)', () => {
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', async () => {
+    return await request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
