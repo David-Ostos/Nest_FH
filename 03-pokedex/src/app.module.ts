0c9ace303875710c6d8082ca9555af5e8cbab61f
David Ostos <davidostos2@gmail.com>
2023-11-18 16:18:07 -0400

    subiendo clase terminada de MongoDB pokedex

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,7 +1,9 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 
 @Module({
@@ -9,7 +11,15 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
-    PokemonModule
+
+    // coneccion a base de datos
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+
+    PokemonModule,
+
+
+    CommonModule
   ]
 })
 export class AppModule {}
--- 03-pokedex/src/pokemon/pokemon.controller.ts
+++ 03-pokedex/src/pokemon/pokemon.controller.ts
@@ -1,34 +1,34 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
-import { CreatePokemonDto } from './dto/create-pokemon.dto';
-import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-
+import { UpdatePokemonDto, CreatePokemonDto } from './dto';
+import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id/parse-mongo-id.pipe';
 @Controller('pokemon')
 export class PokemonController {
-  constructor(private readonly pokemonService: PokemonService) {}
+  constructor (private readonly pokemonService: PokemonService) {}
 
   @Post()
-  create(@Body() createPokemonDto: CreatePokemonDto) {
-    return this.pokemonService.create(createPokemonDto);
+  // @HttpCode(HttpStatus.CREATED) este decorador nos ayuda si queremos especificar el status code
+  async create (@Body() createPokemonDto: CreatePokemonDto) {
+    return await this.pokemonService.create(createPokemonDto);
   }
 
   @Get()
-  findAll() {
-    return this.pokemonService.findAll();
+  async findAll () {
+    return await this.pokemonService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pokemonService.findOne(+id);
+  @Get(':term')
+  async findOne (@Param('term') term: string) {
+    return await this.pokemonService.findOne( term );
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePokemonDto: UpdatePokemonDto) {
-    return this.pokemonService.update(+id, updatePokemonDto);
+  @Patch(':term')
+  async update (@Param('term') term: string, @Body() updatePokemonDto: UpdatePokemonDto) {
+    return await this.pokemonService.update( term, updatePokemonDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pokemonService.remove(+id);
+  async remove (@Param('id', ParseMongoIdPipe) id: string) {
+    return await this.pokemonService.remove( id );
   }
 }
